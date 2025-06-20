0435cc902cba09610a77f2cfdc7766237ae4fca8
manjunath_hm <manjunathcs052@gmail.com>
2025-06-20 07:19:18 +0530

    Code to test the given sudoku problem is valid or not.

---
+++ src/test/java/org/manjunath/interview/arrays/TestValidSudoku.java
@@ -0,0 +1,54 @@
+package org.manjunath.interview.arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestValidSudoku {
+    private ValidSudoku obj;
+
+    @BeforeEach
+    void initialization() {
+        obj = new ValidSudoku();
+    }
+
+    @AfterEach
+    void destroy() {
+        obj = null;
+    }
+
+    @Test
+    void testIsValidSudokuForTure() {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        Assertions.assertTrue(obj.isValidSudoku(board));
+    }
+
+    @Test
+    void testIsValidSudokuForFalse() {
+        char[][] board = {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        Assertions.assertFalse(obj.isValidSudoku(board));
+    }
+
+}
